50fae13973d339097efe32aa09647d9a09665abb
Booflame <Loekken.n@gmail.com>
2022-11-10 13:28:21 +0100

    navlink

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,15 @@
 import '../css/navigation.css';
+import { NavLink } from 'react-router-dom';
 
 export default function Navigation() {
     
     return(
         <nav className='primary-nav'>
             <ul>
-                <li><a href="/">Hej</a></li>
-                <li><a href="/">Heej</a></li>
-                <li><a href="/">Heeej</a></li>
-                <li><a href="/">Heeeej</a></li>
+                <li><NavLink to="/">Hej</NavLink></li>
+                <li><NavLink to="/">Heej</NavLink></li>
+                <li><NavLink to="/">Heeej</NavLink></li>
+                <li><NavLink to="/">Heeeej</NavLink></li>
             </ul>
         </nav>
     )
